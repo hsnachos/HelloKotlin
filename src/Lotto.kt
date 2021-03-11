09679ddae8c78175de69a1945a244552f2def283
hsnachos <hsnachos@gmail.com>
2021-03-11 21:16:39 +0900

    First commit

---
+++ src/Lotto.kt
@@ -0,0 +1,27 @@
+import java.util.*
+
+fun makeLotto():TreeSet<Int>{
+    val lottoTreeSet:TreeSet<Int> = TreeSet()
+    val random = Random()
+
+    while(lottoTreeSet.size < 7){
+        lottoTreeSet.add(random.nextInt(45)+1)
+    }
+    return lottoTreeSet
+}
+
+
+fun main(){
+    println("출력할 로또번호 개수를 입력하세요. (1부터 5사이)")
+    val inputNum = Scanner(System.`in`).nextInt()
+    val lottoList:LinkedList<TreeSet<Int>> = LinkedList()
+
+    if(inputNum in 1..5){
+        for(i in 0 until inputNum) lottoList.add(makeLotto())
+    } else{
+        println("1부터 5사이의 수를 입력해주세요.")
+        return
+    }
+
+    for(i in 0 until lottoList.size) println(lottoList[i])
+}
